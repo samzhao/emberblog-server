4c6e2738c78a897625698b473dc0245b74393eb2
Sam Zhao <zhao6518@gmail.com>
2013-04-09 22:16:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberblogServer::Application.config.secret_token = 'ac1bca9da3cd60145f784bee552812417a6a2c15d4534ced578670267aa6766614ba00f3a3684571b6923037f4fa49f3ac7d9be9cbf0b0bf4fa518433ddae287'
